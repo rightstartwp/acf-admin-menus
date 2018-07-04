69ded9459dc641500cbe3e986bd83b93b68c6a40
moshemo <moshem@live.com>
2018-07-04 11:50:15 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ACF Admin Menus Field
+
+The ACF Admin Menus Field adds a custom field type that pulls in the names of the top level menus from the WordPress admin.
---
+++ acf-admin-menus.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+Plugin Name: Advanced Custom Fields: Admin Menus
+Plugin URI: PLUGIN_URL
+Description: SHORT_DESCRIPTION
+Version: 1.0.0
+Author: AUTHOR_NAME
+Author URI: AUTHOR_URL
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// exit if accessed directly
+if( ! defined( 'ABSPATH' ) ) exit;
+
+
+// check if class already exists
+if( !class_exists('rswp_acf_plugin_admin_menus') ) :
+
+class rswp_acf_plugin_admin_menus {
+	
+	// vars
+	var $settings;
+	
+	
+	/*
+	*  __construct
+	*
+	*  This function will setup the class functionality
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	void
+	*  @return	void
+	*/
+	
+	function __construct() {
+		
+		// settings
+		// - these will be passed into the field class.
+		$this->settings = array(
+			'version'	=> '1.0.0',
+			'url'		=> plugin_dir_url( __FILE__ ),
+			'path'		=> plugin_dir_path( __FILE__ )
+		);
+		
+		
+		// include field
+		add_action('acf/include_field_types', 	array($this, 'include_field')); // v5
+		add_action('acf/register_fields', 		array($this, 'include_field')); // v4
+	}
+	
+	
+	/*
+	*  include_field
+	*
+	*  This function will include the field type class
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	$version (int) major ACF version. Defaults to 4
+	*  @return	void
+	*/
+	
+	function include_field( $version = 4 ) {
+		
+		// load acf-admin-menus
+		load_plugin_acf-admin-menus( 'acf-admin-menus', false, plugin_basename( dirname( __FILE__ ) ) . '/lang' ); 
+		
+		
+		// include
+		include_once('fields/class-rswp-acf-field-admin-menus-v' . $version . '.php');
+	}
+	
+}
+
+
+// initialize
+new rswp_acf_plugin_admin_menus();
+
+
+// class_exists check
+endif;
+	
+?>
